b693c3ca566c795435edb5b5a1134a012b39863e
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2023-04-19 21:15:13 +0100

    updated landing

--- frontend/src/layouts/landingPage/Footer.jsx
+++ frontend/src/layouts/landingPage/Footer.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-import twitter from "../../assets/landing/twitter.svg"
-import telegram from "../../assets/landing/telegram.svg"
-import discord from "../../assets/landing/discord.svg"
-import chat from "../../assets/landing/chat.svg"
-import phone from "../../assets/landing/phone.svg"
-
+import React, { useState } from "react";
+import twitter from "../../assets/landing/twitter.svg";
+import telegram from "../../assets/landing/telegram.svg";
+import discord from "../../assets/landing/discord.svg";
+import chat from "../../assets/landing/chat.svg";
+import phone from "../../assets/landing/phone.svg";
 
 function Footer() {
+  const [copied, setCopied] = useState(false);
+
+  console.log(copied)
+
+  function copyToClipboard() {
+     document.getElementById("+1 (202) 555 0156");
+    setCopied(true);
+  }
   return (
     <div className="flex justify-between items-center">
       <div className="flex gap-[21px]">
@@ -15,12 +22,16 @@ function Footer() {
           <img src={telegram} alt="telegram" />
           <img src={discord} alt="discord" />
         </div>
-        <p className="text-[#FFFFFF]">Copyright © Stable Borrow 2023 | All Rights Reserved</p>
+        <p className="text-[#FFFFFF]">
+          Copyright © Stable Borrow 2023 | All Rights Reserved
+        </p>
       </div>
       <div className="flex text-[#FFFFFF] items-center gap-[30px]">
         <div className="flex gap-[9px] items-center">
           <img src={phone} alt="phone" />
-          <p>+1 (202) 555 0156</p>
+          <p className="cursor-pointer" onClick={copyToClipboard}>
+            +1 (202) 555 0156
+          </p>
         </div>
         <div className="flex gap-[9px] items-center">
           <img src={chat} alt="chat" />
