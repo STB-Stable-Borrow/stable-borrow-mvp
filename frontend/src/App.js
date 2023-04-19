a84d9c1a103325c1a626c462f6e6d49cd9c0e320
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2023-04-19 10:18:20 +0100

    feat:landing page updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
+import React from "react";
 import Index from "./layouts";
 
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-     <Index />
-    </div>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Index />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
   );
 }
-
 export default App;
---
+++ frontend/src/layouts/landingPage/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import twitter from "../../assets/landing/twitter.svg"
+import telegram from "../../assets/landing/telegram.svg"
+import discord from "../../assets/landing/discord.svg"
+import chat from "../../assets/landing/chat.svg"
+import phone from "../../assets/landing/phone.svg"
+
+
+function Footer() {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex gap-[21px]">
+        <div className="flex gap-[13px]">
+          <img src={twitter} alt="twitter" />
+          <img src={telegram} alt="telegram" />
+          <img src={discord} alt="discord" />
+        </div>
+        <p className="text-[#FFFFFF]">Copyright © Stable Borrow 2023 | All Rights Reserved</p>
+      </div>
+      <div className="flex text-[#FFFFFF] items-center gap-[30px]">
+        <div className="flex gap-[9px] items-center">
+          <img src={phone} alt="phone" />
+          <p>+1 (202) 555 0156</p>
+        </div>
+        <div className="flex gap-[9px] items-center">
+          <img src={chat} alt="chat" />
+          <p>info@stableborrow.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
