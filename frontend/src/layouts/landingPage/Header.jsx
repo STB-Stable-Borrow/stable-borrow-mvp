ac7e6bcdf6683dc437a9eb6f540f8e93a8078483
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2023-04-21 14:02:29 +0100

    feat:complete about page

--- frontend/src/layouts/landingPage/Header.jsx
+++ frontend/src/layouts/landingPage/Header.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import logoStb from "../../assets/landing/stb-logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <div className="flex justify-between items-center">
       <div>
-        <img className="w-[259px] h-[59px]" src={logoStb} alt="Stb Logo" />
+        <Link to='/'>
+          <img className="w-[259px] h-[59px]" src={logoStb} alt="Stb Logo" />
+        </Link>
       </div>
       <div className="flex justify-betweeen items-center gap-[41px] text-[#FFFFFF] font-semibold">
         <ul className="flex gap-[41px] font-sans  text-lg cursor-pointer">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/about" className={location.pathname === "/about" ? "text-[#009FBD]" : ""}>About</Link>
           </li>
           <li>
-            <Link to="/exchange">Exchange</Link>
+            <Link to="/exchange" className={location.pathname === "/exchange" ? "text-[#009FBD]" : ""}>Exchange</Link>
           </li>
-          {window.location.pathname !== "/" && (
+          {location.pathname !== "/" && (
             <li>
-              <Link to="/contact">Contact</Link>
+              <Link to="/contact" className={location.pathname === "/contact" ? "text-[#009FBD]" : ""}>Contact</Link>
             </li>
           )}
         </ul>
 
-        {window.location.pathname === "/" && (
+        {location.pathname === "/" && (
           <button className="border border-[#009FBD] px-[17px] py-[10px] rounded-lg">
             Connect Wallet
           </button>
--- frontend/src/pages/Info.jsx
+++ frontend/src/pages/Info.jsx
@@ -1,55 +1,57 @@
 import React from "react";
-import bgImage from "../assets/landing/bg.png";
-import Header from "../layouts/landingPage/Header";
 import borrow from "../assets/info/Borrow.svg";
 import exchange from "../assets/info/Exchange.svg";
 import earn from "../assets/info/Earn.svg";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
+import LandingBody from "../layouts";
+import { useAbout } from "../context/aboutContext";
 
 function Info() {
+  const {navigateToAbout} = useAbout()
+
   return (
-    <div
-      className="min-h-screen w-screen md:px-[80px] md:pt-[37px] md:pb-[10px]     "
-      style={{ backgroundImage: `url(${bgImage})` }}
+    <LandingBody
     >
-      <Header />
       <body className="md:mx-[60px] md:mt-[48px] flex justify-between text-white  min-h-[calc(100vh-180px)] overflow-hidden  ">
         <div className=" flex-col flex justify-between md:w-[45%] ">
-          <div className="border border-[#009FBD]  rounded-[30px] flex flex-col items-center gap-[5px] px-[6px] md:h-[48%]  ">
+          <div className="border border-[#009FBD]  rounded-[30px] flex flex-col items-center gap-[5px] px-[6px] py-2 md:h-[48%]  ">
             <img src={borrow} alt="" className="w-[120px] " />
-            <h1 className="font-bold  underline text-sm">Borrow $STC</h1>
-            <p className="text-xs text-center ">
-              Stable Coin ($STC) is Stable Borrow (STB) Official Token, it’s a
+            <h1 className="font-black border-b-2 border-[#009FBD] text-lg ">Borrow STC</h1>
+            <p className="text-center mx-4 mt-2 text-[#FFFFFF] ">
+              <span className="text-lg font-bold text-white">
+              Stable Coin ($STC)
+              </span> is Stable Borrow (STB) Official Token, it’s a
               fully Decentralized and collateral backed token that’s maintains
               1usd as it’s price making it a Stable Token ...{" "}
-              <Link to="/faqs" className="text-[#865DFF] hover:underline ">
+              <Link to="/about" className="text-[#865DFF] underline underline-offset-2 hover:underline cursor-pointer " onClick={() => navigateToAbout("Borrow")}>
                 learn more
               </Link>
             </p>
-            <button className="w-[120px] h-[30px] bg-[#009FBD] hover:bg-opacity-75 rounded-lg text-xs  ">
+            <button className="bg-[#009FBD] hover:bg-opacity-75 rounded-lg text-lg px-[40px] py-2 mt-2">
               Borrow now
             </button>
           </div>
-          <div className="border border-[#009FBD]  rounded-[30px] flex flex-col items-center gap-[10px] p-[6px] h-[48%]  ">
-            <img src={earn} alt="" className="w-[80px] " />
-            <h1 className="font-bold underline text-sm">Earn $STC</h1>
-            <p className="text-xs text-center ">
-              Register as an haunter today to earn as you play by haunting
+          <div className="border border-[#009FBD]  rounded-[30px] flex flex-col items-center gap-[5px] px-[6px] py-2 md:h-[48%]  ">
+            <img src={earn} alt="" className="w-[120px] " />
+            <h1 className="font-black border-b-2 border-[#009FBD] text-lg ">Earn STC</h1>
+            <p className="text-center mx-4 mt-2 text-[#FFFFFF] ">
+            Register as an haunter today to earn as you play by haunting
               vaults in liquidation and regulating prices. Earn various tokens
               that can be exchanged to any token via exchange UI ...
-              <Link to="/faqs" className="text-[#865DFF] hover:underline ">
+              <Link to="/about" className="text-[#865DFF] underline underline-offset-2 hover:underline " onClick={() => navigateToAbout("Earn")}>
                 learn more
               </Link>
             </p>
-            <button className="w-[120px] h-[30px] bg-[#009FBD] hover:bg-opacity-75 rounded-lg text-xs ">
+            <button className="bg-[#009FBD] hover:bg-opacity-75 rounded-lg text-lg px-[40px] py-2 mt-2">
               Earn now
             </button>
           </div>
         </div>
-        <div className="border border-[#009FBD]  rounded-[30px] flex flex-col items-center gap-[10px] p-[6px] w-[45%] ">
+        <div className="border border-[#009FBD]  rounded-[30px] flex flex-col items-center gap-[10px] py-[6px] px-[20px] w-[45%] ">
           <img src={exchange} alt="" className="w-[80px] " />
-          <h1 className="font-bold underline text-sm">Exchange</h1>
-          <p className="text-xs text-center px-[28px] ">
+          <h1 className="font-black border-b-2 border-[#009FBD] text-lg ">Exchange</h1>
+
+          <p className="text-center mx-4 mt-2 text-[#FFFFFF] ">
             Stable Swap is Stable Borrow Official Exchange, it’s a Decentralized
             Exchange with Unique Protocols that allows users Swap Tokens and
             earn as a Liquidity provider and Pool Creator.
@@ -62,16 +64,16 @@ function Info() {
             <br /> Stable Swap keeps it’s users safe through unique security on
             and off chain to screen each token listed by project owners and take
             damage deposit in advance to keep scam tokens away ...
-            <Link to="/faqs" className="text-[#865DFF] hover:underline ">
+            <Link to="/about" className="text-[#009FBD] hover:underline " onClick={() => navigateToAbout("Exchange")}>
               learn more
             </Link>
           </p>
-          <button className="w-[120px] h-[30px] bg-[#865DFF] hover:bg-opacity-75 rounded-lg text-xs mt-[20px] ">
+          <button className="bg-[#865DFF] hover:bg-opacity-75 rounded-lg text-lg px-[40px] py-2 mt-2">
             Exchange
           </button>
         </div>
       </body>
-    </div>
+    </LandingBody>
   );
 }
 
