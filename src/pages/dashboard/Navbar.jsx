dda7a827e9290568ed382a3c8c336ce49f3fac4b
Mohzcrea8me <mohzcrea8me@gmail.com>
2023-05-29 21:30:39 +0800

    Update Navbar.jsx

--- src/pages/dashboard/Navbar.jsx
+++ src/pages/dashboard/Navbar.jsx
@@ -3,7 +3,7 @@ import copy from "../../assets/dashboard/copy.svg";
 import profile from "../../assets/dashboard/profile.svg";
 import notif from "../../assets/dashboard/notif.svg";
 
-function Navbar({ _account, _address, _profile }) {
+function Navbar({ _account, _address, _profile}) {
   return (
     <div className="flex items-center justify-between w-full">
       <h1 className="font-semibold text-[1.43rem] ">Welcome!</h1>
@@ -25,12 +25,12 @@ function Navbar({ _account, _address, _profile }) {
           )}
           {!_profile && (
             <img
-              src={profile}
-              alt=""
-              className="w-[3.50vw] h-[3.50vw] rounded-full border-[1.5px] border-[#585858] border-dashed "
-            />
-          )}
-          <div className="absolute top-[-3px] right-[-3px] w-[1.56vw] h-[1.56vw] rounded-full bg-[#FF4D00]">
+            src={profile}
+            alt="profile"
+            className="w-[5.2vw] h-[10.4vh] rounded-full border border-full border-2 border-[#009FBD]"
+          />
+            )}
+          <div className="absolute top-0 right-0 w-[25px] h-[25px] rounded-full bg-[#009FBD]">
             <img src={notif} alt="" />
           </div>
         </div>
